194f63860dc20b226715428b45059dcb589a508a
Dominika5505 <dominikasedilekova@gmail.com>
2019-11-18 21:34:52 +0100

    dashboard redirect

--- auth.js
+++ auth.js
@@ -1,9 +1,9 @@
 module.exports = {
-  ensureAuthenticated: function(req, res, next) {
+  ensureAuthenticated: function (req, res, next) {
     if (req.isAuthenticated()) {
       return next();
     }
     req.flash("error_msg", "Sem nemáš prístup");
-    res.send("Sem nemáš prístup");
+    res.redirect("/");
   }
 };
